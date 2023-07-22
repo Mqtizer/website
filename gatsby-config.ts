3a3c197ead0fc2be14d810ca87e19234dddd6943
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-07-23 01:18:11 +0200

    Added path prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
+  pathPrefix: `/website`,
   siteMetadata: {
     title: `MQTIZER | MQTT Client`,
     siteUrl: `https://www.mqtizer.com`,
