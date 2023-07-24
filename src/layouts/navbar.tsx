00b471e73ad7ed33c6d671196dc9a4d8a40ac114
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-07-24 18:07:48 +0200

    Responsive Nav Bar

--- src/layouts/navbar.tsx
+++ src/layouts/navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 import styled from '@emotion/styled'
 import { Link } from 'gatsby'
@@ -34,6 +34,7 @@ const NavBarContainer = styled.div`
   width: 100vw;
   background-color: rgba(255, 253, 250, 0.9);
   background-filter: blur(20px);
+
   &.shadow {
     transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1);
     box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
@@ -50,32 +51,102 @@ const LogoLink = styled(Link)`
     font-weight: 500;
   }
 `
+const HamBurger = styled.button`
+  width: 40px;
+  border: none;
+  background: none;
+  cursor: pointer;
+  display: none;
+  padding: 5px;
+  @media only screen and (max-width: 920px) {
+    display: block;
+  }
+  // nothing on hover
+  &:hover {
+    background: none;
+    box-shadow: none;
+  }
+`
+const NavLinks = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2rem;
+  @media only screen and (max-width: 920px) {
+    display: none;
+  }
+`
+
+const DownloadButton = styled.button`
+  @media only screen and (max-width: 920px) {
+    display: none;
+  }
+`
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: -100%;
+  padding: 2rem;
+  background-color: rgba(255, 253, 250, 1);
+  z-index: 100;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+  opacity: 1;
+  width: 100%;
+  transition: all 0.2s ease-in-out;
+  box-shadow: var(--elevation-light);
+  &.visible {
+    opacity: 1;
+    left: 0;
+  }
+  @media only screen and (min-width: 920px) {
+    display: none;
+  }
+  & > button {
+    width: 80%;
+    margin: 0 auto;
+  }
+`
+
 export type NavBarProps = {
   openDownloadCTAClick: () => void
 }
 
 export default function Navbar({ openDownloadCTAClick }: NavBarProps) {
   const scrollPosition = useScrollPosition()
+  const [isOverlayVisible, setOverlayVisible] = useState(false)
   return (
     <NavBarContainer className={scrollPosition > 215 ? 'shadow' : ''}>
-      <LogoLink to="/" key="logo">
-        <StaticImage quality={100} src="../images/icon.png" alt="Logo" height={44} />
-        <p>MQTIZER</p>
-      </LogoLink>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          gap: '2rem',
-        }}
-      >
+      <Logo />
+      <NavLinks>
         {NAVIGATION_LINKS.map(link => (
           <StyledLink to={link.path} key={link.name}>
             {link.name}
           </StyledLink>
         ))}
-      </div>
-      <button onClick={() => openDownloadCTAClick()}>
+      </NavLinks>
+      <HamBurger
+        onClick={() => {
+          setOverlayVisible(true)
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          shapeRendering="geometricPrecision"
+          textRendering="geometricPrecision"
+          imageRendering="optimizeQuality"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          viewBox="0 0 512 351.67"
+        >
+          <path fillRule="nonzero" d="M0 0h512v23.91H0V0zm0 327.76h512v23.91H0v-23.91zm0-163.88h512v23.91H0v-23.91z" />
+        </svg>
+      </HamBurger>
+      <DownloadButton onClick={() => openDownloadCTAClick()}>
         Download
         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
@@ -83,7 +154,73 @@ export default function Navbar({ openDownloadCTAClick }: NavBarProps) {
             fill="white"
           />
         </svg>
-      </button>
+      </DownloadButton>
+      <Overlay className={isOverlayVisible ? 'visible' : ''}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <Logo />
+          <HamBurger
+            onClick={() => {
+              setOverlayVisible(false)
+            }}
+          >
+            <svg
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 122.878 122.88"
+              enableBackground="new 0 0 122.878 122.88"
+            >
+              <g>
+                <path d="M1.426,8.313c-1.901-1.901-1.901-4.984,0-6.886c1.901-1.902,4.984-1.902,6.886,0l53.127,53.127l53.127-53.127 c1.901-1.902,4.984-1.902,6.887,0c1.901,1.901,1.901,4.985,0,6.886L68.324,61.439l53.128,53.128c1.901,1.901,1.901,4.984,0,6.886 c-1.902,1.902-4.985,1.902-6.887,0L61.438,68.326L8.312,121.453c-1.901,1.902-4.984,1.902-6.886,0 c-1.901-1.901-1.901-4.984,0-6.886l53.127-53.128L1.426,8.313L1.426,8.313z" />
+              </g>
+            </svg>
+          </HamBurger>
+        </div>
+        {NAVIGATION_LINKS.map(link => (
+          <StyledLink
+            to={link.path}
+            key={link.name}
+            onClick={() => {
+              setOverlayVisible(false)
+            }}
+          >
+            {link.name}
+          </StyledLink>
+        ))}
+        <button
+          style={{
+            width: '80%',
+            margin: '0 auto',
+            padding: '1rem',
+            fontSize: '1.25rem',
+            lineHeight: '1.5rem',
+          }}
+          onClick={() => openDownloadCTAClick()}
+        >
+          Download
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M14.9997 12.5V15H4.99967V12.5H3.33301V15C3.33301 15.9167 4.08301 16.6667 4.99967 16.6667H14.9997C15.9163 16.6667 16.6663 15.9167 16.6663 15V12.5H14.9997ZM14.1663 9.16668L12.9913 7.99168L10.833 10.1417V3.33334H9.16634V10.1417L7.00801 7.99168L5.83301 9.16668L9.99967 13.3333L14.1663 9.16668Z"
+              fill="white"
+            />
+          </svg>
+        </button>
+      </Overlay>
     </NavBarContainer>
+  )
+}
+function Logo() {
+  return (
+    <LogoLink to="/" key="logo">
+      <StaticImage quality={100} src="../images/icon.png" alt="Logo" height={44} />
+      <p>MQTIZER</p>
+    </LogoLink>
   )
 }
