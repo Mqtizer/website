16e5cb99033e36619696289ad4b69ad7a821c800
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-07-26 17:40:29 +0200

    Fix scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,42 +3,41 @@ import * as React from 'react'
 
 import MainLayout from '../layouts'
 
+import styled from '@emotion/styled'
 import { Faq, Features, HeroSection, QuestionSectionFooter, Why } from '../components/homepage'
+
+const HomePageContainer = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: start;
+  flex-direction: column;
+  width: 100vw;
+  gap: 2rem;
+  .main-section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 80vw;
+    margin: 0 auto;
+    gap: 6rem;
+    max-width: 1360px;
+    min-width: 320px;
+    margin-top: 4rem;
+  }
+`
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <MainLayout footerChildren={<QuestionSectionFooter />}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'start',
-          alignItems: 'start',
-          flexDirection: 'column',
-          width: '100vw',
-          gap: '2rem',
-        }}
-      >
+      <HomePageContainer>
         <HeroSection />
-        <div
-          style={{
-            paddingTop: '2rem',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            width: '80vw',
-
-            margin: '0 auto',
-            gap: '6rem',
-            maxWidth: '1360px',
-            minWidth: '320px',
-          }}
-        >
+        <div className="main-section">
           <Why />
           <Features />
           <Faq />
         </div>
-      </div>
+      </HomePageContainer>
     </MainLayout>
   )
 }
